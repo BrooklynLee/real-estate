036b8c332264ba14204cce0b81203cf7b31291de
stato <direcision@gmail.com>
2019-06-21 23:03:06 +0900

    add data_loader

--- src/util.py
+++ src/util.py
@@ -8,7 +8,7 @@ def read_csv(filepath):
     return pd.read_csv(os.path.join(DATA_PATH, filepath), sep="\t")
 
 def send_msg_to_slack(text):
-    url = "<hook_url>"
+    url = open("../config/slack.cfg", "r").readlines()[0][:-1]
     payload = "{\n\t\"text\": \""+text+"\"}"
     headers = {
         'content-type': "application/json",
@@ -18,3 +18,38 @@ def send_msg_to_slack(text):
 
     response = requests.request("POST", url, data=payload, headers=headers)
     print(response.text)
+
+# READ RCODES
+def get_rcode_df():
+    f_in = file('../data/road_code.csv', 'r')
+    lines = [x[:-1].split(',') for x in f_in.readlines()]
+
+    from collections import defaultdict
+    r_dict = defaultdict() 
+    for line in lines: 
+        r_dict[line[0]] = [line[4], line[5], line[2]]
+
+    import pandas as pd
+    r_df = pd.DataFrame.from_dict(r_dict)
+    r_df = r_df.transpose()
+    # r_df.head(10) # metadata
+    return r_df
+
+def load_data(rcode, trade_type):
+    import glob
+    filelist = glob.glob("../data/apt-{}/{}/*.csv".format(trade_type, rcode))
+    list_ = []
+    for file in filelist:
+        df = pd.read_csv(file,index_col=None,header=0)
+        list_.append(df)
+    item_df = pd.concat(list_)
+    return item_df
+
+def get_data_by_trade_type(rcodes, trade_type):
+    item_df_list = []
+    for rcode in rcodes:
+        item_df = load_data(rcode, trade_type) 
+        # print '{}: {}'.format(r_df.loc[rcode], len(item_df))
+        item_df_list.append(item_df)
+    df = pd.concat(item_df_list)
+    return df
