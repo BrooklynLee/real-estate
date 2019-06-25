798b7b4ab3bced5d08ef85075e5d389a824fd499
stato <direcision@gmail.com>
2019-06-25 21:47:58 +0900

    (#1) fix bug - using passed index

--- src/es_client.py
+++ src/es_client.py
@@ -16,11 +16,6 @@ class ESClient():
     def __init__(self):
         self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
         self.es.indices.refresh(index="real-estate")
-        self.s = Search(using=self.es)
-
-    def _bulk(self, docs):
-        res = helpers.bulk(self.es, docs)
-        print (res)
 
     def _put(self, index, dtype, id, doc):
         res = self.es.index(index=index, doc_type=dtype, id=id, body=doc)
@@ -36,18 +31,21 @@ class ESClient():
         return res
 
     def _search_by_date(self, index, date):
+        self.s = Search(using=self.es, index=index)
         q = Q('bool', must=[Q('match', date=date)])
         s = self.s.query(q)
         resp = s.execute()
         return self.result2df(resp)
 
     def _search_by_aptname(self, index, aptname):
+        self.s = Search(using=self.es, index=index)
         q = Q('bool', must=[Q('match', 아파트=aptname)])
         s = self.s.query(q)
         resp = s.execute()
         return self.result2df(resp)
 
     def _search_by_rcode(self, index, rcode):
+        self.s = Search(using=self.es, index=index)
         q = Q('bool', must=[Q('match', 지역코드=rcode)])
         s = self.s.query(q)
         # resp = s.execute()
@@ -56,6 +54,7 @@ class ESClient():
 
     # https://elasticsearch-dsl.readthedocs.io/en/latest/search_dsl.html#pagination
     def _search_by_query(self, index, q):
+        self.s = Search(using=self.es, index=index)
         s = self.s.query(q)
         resp = s.scan() # to access all documents
         # resp = s.execute()
